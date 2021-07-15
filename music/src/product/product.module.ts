3e9856a1bd19a201c224acb9f4be2d836d133902
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-07-16 01:02:23 +0900

    Add Products CRUD

--- music/src/product/product.module.ts
+++ music/src/product/product.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
+import { Product } from './product.entity';
+import { ProductService } from './product.service';
 
 @Module({
-  controllers: [ProductController]
+  imports: [
+    TypeOrmModule.forFeature([Product])
+  ],
+  controllers: [ProductController],
+  providers: [ProductService]
 })
-export class ProductModule {}
+export class ProductModule { }
