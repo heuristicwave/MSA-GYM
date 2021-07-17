13bc27228be359998cc7e415e73d3be8e482ae32
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-07-17 16:05:46 +0900

    Add Nest Config

--- main/src/app.module.ts
+++ main/src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest_main', {
-      autoCreate: true
-    }),
+  imports: [ConfigModule.forRoot(),
+  MongooseModule.forRoot('mongodb://localhost:27017/nest_main', {
+    autoCreate: true
+  }),
     ProductModule,
   ],
   controllers: [AppController],
--- music/src/app.module.ts
+++ music/src/app.module.ts
@@ -3,19 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '1234',
-      database: 'music',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
+  imports: [ConfigModule.forRoot(),
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '1234',
+    database: 'music',
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
     ProductModule],
   controllers: [AppController],
   providers: [AppService],
