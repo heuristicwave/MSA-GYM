b31dacbde40431407b238498676e4280908660f2
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-07-22 00:05:18 +0900

    Add MainApp Front

--- msa-front/src/App.tsx
+++ msa-front/src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import Menu from "./components/Menu";
 import Nav from "./components/Nav";
+import Main from "./main/Main";
 import Products from "./admin/Products";
 import { BrowserRouter, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ function App() {
           <Menu />
           <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
             <BrowserRouter>
+              <Route path="/" component={Main} />
               <Route path="/admin/products" component={Products} />
             </BrowserRouter>
           </main>
