6304d36c7d69d96e155ac1722923a70b127ebd22
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-07-25 15:37:57 +0900

    Add Wrapper

--- msa-front/src/App.tsx
+++ msa-front/src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Menu from "./components/Menu";
-import Nav from "./components/Nav";
 import Main from "./main/Main";
 import Products from "./admin/Products";
 import { BrowserRouter, Route } from "react-router-dom";
@@ -10,18 +8,10 @@ import { BrowserRouter, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <BrowserRouter>
-              <Route path="/" component={Main} />
-              <Route path="/admin/products" component={Products} />
-            </BrowserRouter>
-          </main>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route path="/" exact component={Main} />
+        <Route path="/admin/products" component={Products} />
+      </BrowserRouter>
     </div>
   );
 }
