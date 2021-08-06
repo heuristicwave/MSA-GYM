50b86f43adb710ca8f0994faaba79cb1fc81c6d5
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-08-06 23:25:23 +0900

    Add react hook

--- msa-front/src/admin/Products.tsx
+++ msa-front/src/admin/Products.tsx
@@ -1,29 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Wrapper from "./Wrapper";
 
 const Products = () => {
+  // 19:53 부터
+  useEffect(() => {
+    /*
+    const getProduct = async () => {
+    };
+    getProduct();
+    */
+
+    (async () => {
+      const response = await fetch("http://localhost:8000/api/products");
+      const data = await response.json();
+
+      console.log(data);
+    })();
+  }, []);
+
   return (
-    <div className="table-responsive">
-      <table className="table table-striped table-sm">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-            <th scope="col">Header</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1,001</td>
-            <td>random</td>
-            <td>data</td>
-            <td>placeholder</td>
-            <td>text</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <Wrapper>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+              <th scope="col">Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+              <td>placeholder</td>
+              <td>text</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </Wrapper>
   );
 };
 
